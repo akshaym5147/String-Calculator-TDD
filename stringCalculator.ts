10ff7012b45a2325db38b7ba5105f4b15c5c50b6
akshaym5147 <akshaymuge888@gmail.com>
2025-09-23 18:23:11 +0530

    feat - handled negetive numbers case

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -9,17 +9,33 @@ const stringCalculator = (numbers: string) => {
     }
 
     const numbersSplit = copyNumbers.split(/,|\n/);
-    const sum = numbersSplit.reduce((res: number, currentVal: string) => res += parseInt(currentVal), 0); 
+
+    let sum = 0;
+    const negetiveNumbers: string[] = [];
+    // I am adding negetive numbers as well in sum
+    // I will throw the errors if negetiveNumbers exist and will return null
+    numbersSplit.forEach((item) => {
+        if(parseInt(item) < 0) {
+            negetiveNumbers.push(item)
+        } else {
+            sum += parseInt(item);
+        }
+    })
+
+    if(negetiveNumbers.length > 0) {
+        console.error("negatives not allowed:", negetiveNumbers.join(','))
+        return;
+    }
     return sum;
 }
 
 const sumOfCommaSeparatedNums = stringCalculator("1,5");
+console.log('sumOfCommaSeparatedNums', sumOfCommaSeparatedNums);
 const sumOfLargeNumbersCommaSeparated = stringCalculator("2,6,0,7,9,1,0,3,4,8,1,1,9,2,1,8,1,3,5,3,1,8,3,7,4,0,5,0,9,7,4,3,8,4,7,5,8,8,9,2,1,7,2,1,5,6,0,4,6,1,4,1,9,0,3,0,2,7,8,8,0,9,5,9,9,0,1,3,0,2,3,7,8,2,5,9,0,3,9,5,6,7,9,7,2,0,8,8,9,3,9,9,1,9,7,9,8,9,1,9");
+console.log('sumOfLargeNumbersCommaSeparated', sumOfLargeNumbersCommaSeparated);
 const sumOfNumsWithNewLines = stringCalculator("1\n2,3");
+console.log('sumOfNumsWithNewLines', sumOfNumsWithNewLines);
 const sumOfDelimiters = stringCalculator("//;\n1;2;3");
-console.log('ANSWER -', {
-    sumOfCommaSeparatedNums,
-    sumOfLargeNumbersCommaSeparated,
-    sumOfNumsWithNewLines,
-    sumOfDelimiters
-});
+console.log('sumOfDelimiters', sumOfDelimiters);
+const withNegetiveNumbers = stringCalculator("-1,5");
+console.log('withNegetiveNumbers', withNegetiveNumbers);
