da8d4905a1ef68fcaa7a32d69bf9ceb45cb0e7b3
akshaym5147 <akshaymuge888@gmail.com>
2025-09-23 21:15:35 +0530

    feat - handled multiple types of Delimiters and merged with existing Delimiters check

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,19 +1,34 @@
-const DELIMITERS_REGEX = /^\/\/(.+)\n(.*)$/;
+const MULTI_DELIMITERS_REGEX = /^\/\/(\[.*?\]+)\n(.*)$/;
+const DELIMITER_PATTERN_REGEX = /\[(.*?)\]/g;
+
+const getEscapedRegEx = (str?: string) => {
+    return str?.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
 const stringCalculator = (numbers: string) => {
     let copyNumbers = numbers;
-    const regMatch = copyNumbers.match(DELIMITERS_REGEX);
+    const regMultiDelimitersMatch = copyNumbers.match(MULTI_DELIMITERS_REGEX);
 
-    if(regMatch && regMatch[1]) {
-        copyNumbers = regMatch[2] || copyNumbers;
-        copyNumbers = copyNumbers.replace(new RegExp(regMatch[1], 'g'), ',');
+    if(regMultiDelimitersMatch) {
+        const delimiters = regMultiDelimitersMatch[1] || '';
+        const delimitersList = [
+            ...delimiters.matchAll(DELIMITER_PATTERN_REGEX).map(r => r[1])
+        ];
+
+        const escapedRegex = delimitersList.map(getEscapedRegEx)
+        const delimiterExtracterRegex = new RegExp(escapedRegex.join('|'));
+
+        copyNumbers = regMultiDelimitersMatch[2] || copyNumbers;
+        copyNumbers = copyNumbers.replace(new RegExp(delimiterExtracterRegex, 'g'), ',');
     }
 
     const numbersSplit = copyNumbers.split(/,|\n/);
 
     let sum = 0;
     const negetiveNumbers: string[] = [];
-    // I am adding negetive numbers as well in sum
-    // I will throw the errors if negetiveNumbers exist and will return null
+    // I am adding negetive numbers in seperate array since its mentioned to print all negetive numbers
+    // I am keeping track of positive nums sum as well
+    // If negetive numbers are present then printing  errors and returning null
     numbersSplit.forEach((item) => {
         if(parseInt(item) < 0) {
             negetiveNumbers.push(item)
@@ -29,18 +44,26 @@ const stringCalculator = (numbers: string) => {
     return sum;
 }
 
-const sumOfCommaSeparatedNums = stringCalculator("1,5");
-console.log('sumOfCommaSeparatedNums', sumOfCommaSeparatedNums);
-const sumOfLargeNumbersCommaSeparated = stringCalculator("2,6,0,7,9,1,0,3,4,8,1,1,9,2,1,8,1,3,5,3,1,8,3,7,4,0,5,0,9,7,4,3,8,4,7,5,8,8,9,2,1,7,2,1,5,6,0,4,6,1,4,1,9,0,3,0,2,7,8,8,0,9,5,9,9,0,1,3,0,2,3,7,8,2,5,9,0,3,9,5,6,7,9,7,2,0,8,8,9,3,9,9,1,9,7,9,8,9,1,9");
-console.log('sumOfLargeNumbersCommaSeparated', sumOfLargeNumbersCommaSeparated);
-const sumOfNumsWithNewLines = stringCalculator("1\n2,3");
-console.log('sumOfNumsWithNewLines', sumOfNumsWithNewLines);
-const sumOfDelimiters = stringCalculator("//;\n1;2;3");
-console.log('sumOfDelimiters', sumOfDelimiters);
-const withNegetiveNumbers = stringCalculator("-1,5");
-console.log('withNegetiveNumbers', withNegetiveNumbers);
-const withNumbersGtrThan1000 = stringCalculator("1,1000,1001");
-console.log('withNumbersGtrThan1000', withNumbersGtrThan1000);
+const commaSeperatedNums = stringCalculator("1,5");
+console.log('commaSeperatedNums', commaSeperatedNums);
 
-const withNSizeDelimiter = stringCalculator("//[***]\n1***2***3");
-console.log('withNSizeDelimiter', withNSizeDelimiter);
+const largeCommaSeperatedNums = stringCalculator("2,6,0,7,9,1,0,3,4,8,1,1,9,2,1,8,1,3,5,3");
+console.log('largeCommaSeperatedNums', largeCommaSeperatedNums);
+
+const numsWithNewLines = stringCalculator("1\n2,3");
+console.log('numsWithNewLines', numsWithNewLines);
+
+const numsWithSingleTypeDelimiters = stringCalculator("//;\n1;2;3");
+console.log('numsWithSingleTypeDelimiters', numsWithSingleTypeDelimiters);
+
+const numsWithNegetiveNumbers = stringCalculator("-1,5");
+console.log('numsWithNegetiveNumbers', numsWithNegetiveNumbers);
+
+const numsWithGtrThan1000 = stringCalculator("1,1000,1001");
+console.log('numsWithGtrThan1000', numsWithGtrThan1000);
+
+const numsWithNSizeDelimiter = stringCalculator("//[***]\n1***2***3");
+console.log('numsWithNSizeDelimiter', numsWithNSizeDelimiter);
+
+const numsWithMultipleTypesDelimiters = stringCalculator("//[*][%]\n1*2%3");
+console.log('numsWithMultipleTypesDelimiters', numsWithMultipleTypesDelimiters);
